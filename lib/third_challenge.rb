f18357ffae4e4eb8b9067813a32e12db9b33018c
steven nguyen <sqnguyen829@gmail.com>
2019-09-12 01:50:13 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Nontague",
+        :name => "Lord Montague",
         :age => "53"
         },
       :matriarch => {
