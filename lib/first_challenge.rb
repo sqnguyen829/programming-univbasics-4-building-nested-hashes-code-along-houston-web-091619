fb2245026c5dfbc18df8545305576ce061c7aa91
steven nguyen <sqnguyen829@gmail.com>
2019-09-12 02:09:08 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,5 +1,62 @@
-  
+def first_challenge
+   epic_tragedy = {
+     :montague => {},
+     :capulet => {}
+   }
+end  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+
 def first_challenge
   # epic_tragedy = {}
   epic_tragedy = {:montague => {}, :capulet => {}}
 end
+
+
+
+
+
+
+
+=end
+
+
+
--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,6 +5,73 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
+          {name: "Benvolio", age: "17" , attitude: "worried"},
+          {name: "Mercutio", age: "18" , attitude: "hot-headed"}
+      ]
+      
+   }, 
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+        {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
+      ]
+   }
+}
+
+  
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+
+def fourth_challenge
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
          {name: "Mercutio", age: "18", attitude: "hot-headed"},
          {name: "Benvolio", age: "17", attitude: "worried"}
       ]
@@ -20,3 +87,6 @@ def fourth_challenge
    }
   }
 end
+
+
+=end
